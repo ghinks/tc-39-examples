7482c165c886941568cec9682a78b4b65a410a2a
glenn vincent hinks <ghinks@yahoo.com>
2018-07-14 10:46:13 -0400

    working stage-2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import { promises as fs } from 'fs'
 console.log('hello')
+
+const readFileContents = async () => {
+  const file = await fs.readFile('package.json', 'utf8')
+  console.log(file)
+}
+
+readFileContents()
+
